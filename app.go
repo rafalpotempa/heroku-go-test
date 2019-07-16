1bba690a47438e007e519fbb1a434e8857d4926d
Rafal Potempa <rafal.potem@gmail.com>
2019-07-16 15:04:59 +0200

    Remove user and repo

--- app.go
+++ app.go
@@ -20,14 +20,14 @@ type webhook struct {
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	payload, err := github.ValidatePayload(r, []byte("my-secret-key"))
 	if err != nil {
-		log.Printf("error validating request body: err=%s\n", err)
+		log.Printf("error validating request body: err=\"%s\"\n", err)
 		return
 	}
 	defer r.Body.Close()
 
 	event, err := github.ParseWebHook(github.WebHookType(r), payload)
 	if err != nil {
-		log.Printf("could not parse webhook: err=%s\n", err)
+		log.Printf("could not parse webhook: err=\"%s\"\n", err)
 		return
 	}
 
@@ -35,16 +35,16 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	case *github.PushEvent:
 
 	case *github.WatchEvent:
-		// log.Printf("%v", e)
+		log.Printf("%s is watching repo \"%s\"\n", e.GetSender(), e.GetRepo())
 	case *github.StarEvent:
 		// someone starred our repository
 		if e.GetAction() == "created" {
-			log.Printf("%s: %s starred repository %s\n", e.GetStarredAt(), e.GetSender(), e.GetRepo())
+			log.Printf("%s repository starred\n", e.GetStarredAt())
 		} else if e.GetAction() == "delete" {
-			log.Printf("%s: %s unstarred repository %s\n", e.GetStarredAt(), e.GetSender(), e.GetRepo())
+			log.Printf("%s repository unstarred\n", e.GetStarredAt())
 		}
 	default:
-		log.Printf("unknown event type %s\n", github.WebHookType(r))
+		log.Printf("unknown event type: \"%s\"\n", github.WebHookType(r))
 		return
 	}
 }
@@ -54,7 +54,6 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-
 	log.Println("server started")
 	http.HandleFunc("/webhook", handleWebhook)
 	http.HandleFunc("/", index)
